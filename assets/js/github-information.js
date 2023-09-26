a3f58d998aa19df3c0bc6d28dd3ff046b60af50e
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-09-26 18:11:28 +0100

    Implement emailjs integration on contact page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -39,7 +39,7 @@ function repoInformationHTML(repos) {
 function fetchGitHubInformation(event) {
     $("#gh-user-data").html("");
     $("#gh-repo-data").html("");
-    
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -55,15 +55,19 @@ function fetchGitHubInformation(event) {
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(firstResponse, secondResponce) {
+        function(firstResponse, secondResponse) {
             var userData = firstResponse[0];
-            var repoData = secondResponce[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
-        }, function(errorResponse) {
+        },
+        function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
---
+++ assets/js/sendEmail.js
@@ -0,0 +1,22 @@
+function sendMail(contactForm) {
+    emailjs.send("service_nidoi1z","template_lviaozc", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
+
+
+
+
+
